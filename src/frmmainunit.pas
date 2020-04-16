543c108a972c66ae1d10e3fae95587e29912d1ea
Gustavo Carreno <guscarreno@gmail.com>
2020-04-16 22:30:20 +0100

    refactor: Adding dependencies on Synapse and VirtualTreeView
    This will break the build-test

--- src/frmmainunit.pas
+++ src/frmmainunit.pas
@@ -10,15 +10,21 @@ uses
 , Forms
 , Controls
 , Graphics
-, Dialogs, ExtCtrls
+, Dialogs
+, ExtCtrls, StdCtrls
+, httpsend
+, VirtualTrees
 , ResourceUnit;
 
 type
 
   { TfrmMain }
-
-  TfrmMain = class(TForm)
+    TfrmMain = class(TForm)
     Panel1: TPanel;
+    VirtualStringTree1: TVirtualStringTree;
+    Panel2: TPanel;
+      Button1: TButton;
+    procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
 
@@ -38,6 +44,27 @@ implementation
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
   Panel1.Caption:= rsTitle;
+end;
+
+procedure TfrmMain.Button1Click(Sender: TObject);
+var
+  http: THTTPSend;
+begin
+  try
+    Button1.Enabled:= False;
+    http:= THTTPSend.Create;
+    try
+      http.HTTPMethod('GET', 'http://packages.lazarus-ide.org/packagelist.json');
+    except
+      on e: Exception do
+      begin
+        ShowMessage(e.Message);
+      end;
+    end;
+  finally
+    http.Free;
+    Button1.Enabled:= True
+  end;
 end;
 
 end.
